a0a578ef8ff1dbb86baddb965f6147b5cd560538
Juri Lucci <40626240+jurilucci@users.noreply.github.com>
2022-01-04 11:48:47 +0100

    section 5

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { EventDashboard } from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import EventForm from "../../features/events/eventForm/EventForm";
+import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage";
+
+function App() {
+  return (
+    <>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar />
+            <Container className='main'>
+              <Route exact path='/events' component={EventDashboard} />
+              <Route path='/events/:id' component={EventDetailedPage} />
+              <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
+            </Container>
+          </>
+        )}
+      />
+    </>
+  );
+}
+
+export default App;
